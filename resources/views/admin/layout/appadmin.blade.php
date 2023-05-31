eb2afaf701457657990152cbef3abf6443c15e8b
abrarradan30 <abradan84@gmail.com>
2023-05-31 18:31:22 +0700

    add alert laravel

--- resources/views/admin/layout/appadmin.blade.php
+++ resources/views/admin/layout/appadmin.blade.php
@@ -1,10 +1,11 @@
+@include('sweetalert::alert')
 @include('admin.layout.top')
 @include('admin.layout.menu')
 
 <div id="layoutSidenav_content">
                 <main>
                     <div class="container-fluid px-4">
-                        
+                            @include('sweetalert::alert')
                         @yield('content')
                         <!-- yield adalah deklarasi yang akan diisi konten ketika yieldnya dipanggil
                         didalam konten masing-masing, contoh yield yang diatas menggunakan value content -->
