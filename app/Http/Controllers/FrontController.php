444593e1b461be11a56bd83a4b871c8b39487546
abrarradan30 <abradan84@gmail.com>
2023-06-24 23:15:54 +0700

    modified controller

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -11,15 +11,4 @@ class FrontController extends Controller
     {
         return view('front');
     }
-    public function create()
-    {
-        //
-        $pemilik_kos = DB::table('pemilik_kos')->get();
-        $data_kos = DB::table('data_kos')
-            ->join('pemilik_kos', 'data_kos.pemilik_kos_id', '=', 'pemilik_kos.id')
-            ->select('data_kos.*', 'pemilik_kos.nama as nama_pemilik_kos')
-            ->get();
-
-        return view('admin.data_kos.create', compact('data_kos', 'pemilik_kos'));
-    }
 }
