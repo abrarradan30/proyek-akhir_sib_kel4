b11f3bf49671fcb5c5d12288aba3936040ea52eb
Keirs68 <putrimaulida1070@gmail.com>
2023-06-25 23:33:29 +0700

    info-syarat

--- resources/views/info.blade.php
+++ resources/views/info.blade.php
@@ -10,22 +10,13 @@
     <body>
         <section class="about-details section-padding">
       <div class="container">
-        <div class="row">
+        <div class="row justify-content-md-center">
           <div class="col-xs-12 col-md-6" data-aos="fade-right">
-            <div class="single_about_details">
-              <h3>Info</h3>
-              <span></span>
-              <p class="mb20"></p>
-              <p>Ngalamkos memanfaatkan teknologi untuk berkembang dari aplikasi cari kos menjadi aplikasi yang 
+              <h3 class="text-center">Info</h3><br>
+              <p class="text-center">Ngalamkos memanfaatkan teknologi untuk berkembang dari aplikasi cari kos menjadi aplikasi yang 
                 memudahkan calon anak kos untuk sewa properti kos dan juga melakukan pembayaran kos. </p> 
-              <p> Platform yang menyajikan informasi mengenai kost yang disewakan dengan dilengkapi dengan harga kost, fasilitas kost dan gambar kost yang diunggah langsung oleh pemilik kost.
+              <p class="text-center"> Platform yang menyajikan informasi mengenai kost yang disewakan dengan dilengkapi dengan harga kost, fasilitas kost dan gambar kost yang diunggah langsung oleh pemilik kost.
                  Platform kami memberikan kemudahan bagi kamu yang mencari info kost terdekat di sekitaran daerah Kabupaten/Kota Malang yang kamu inginkan. </p>
-            </div>
-          </div><!-- END COL-->
-          <div class="col-md-5 col-md-offset-1 col-xs-12">
-            <!-- <div class="single_about_details_img">
-              <img class="img-responsive" src="http://ekramit.net/tf/kaniz/assets/img/desktop2.png" alt="">
-            </div> -->
           </div><!-- END COL-->
         </div><!-- END ROW-->
       </div><!-- END CONTAINER-->
