8c2c1e2b631e48ab0672428f0cc887bc874a4299
abrarradan30 <abradan84@gmail.com>
2023-06-26 09:41:02 +0700

    modified user auth

--- resources/views/admin/layout/top.blade.php
+++ resources/views/admin/layout/top.blade.php
@@ -21,7 +21,7 @@
     <body>
         <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
             <!-- Navbar Brand-->
-            <a class="navbar-brand ps-3" href="{{url('admin/dashboard')}}" style="color: blue;"><b>ngalamkos</b></a>
+            <a class="navbar-brand ps-3" href="{{url('admin/dashboard')}}" style="color: blue; font-size: 30px;"><b>ngalamkos</b></a>
             <!-- Sidebar Toggle-->
             <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
             <!-- Navbar Search-->
