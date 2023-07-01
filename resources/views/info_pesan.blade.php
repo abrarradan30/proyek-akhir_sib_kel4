39b7457752be0763c9bab43ba619dc70dc60c684
abrarradan30 <abradan84@gmail.com>
2023-07-01 18:39:11 +0700

    modified views

--- resources/views/info_pesan.blade.php
+++ resources/views/info_pesan.blade.php
@@ -11,19 +11,19 @@
         <section class="about-details section-padding">
       <div class="container">
         <div class="row">
-          <div class="col-xs-12 col-md-6" data-aos="fade-right">
+          <div class="col-xs-12 col-md-10" data-aos="fade-right">
             <div class="single_about_details">
               <h3>Terimakasih sudah melakukan sewa kos</h3>
               <p></p>
               <span></span>
               <p class="mb20"></p>
-              <p>Pesanan anda akan diproses maksimal dalam 1x24 jam</p> 
+              <p>Jika belum melakukan pembayaran harap menghubungi pemilik kos terkait pembayaran,</p> 
+              <p>pada nomer tertera di detail kos, kemudian lakukan pembayaran ulang pada riwayat pesanan</p>
+              <p>dan konfirmasi kepada pemilik kos, sehingga pesanan anda akan diproses maksimal dalam 1x24 jam</p>
               <br>
-              <p>Info status pembayaran kos anda,</p>
-              <p>dapat anda lihat pada bagian riwayat pesanan</p>
-              <br>
-              <p>Harap menghubungi pemilik kos terkait pembayaran,</p>
-              <p>pada nomer tertera di detail kos</p>
+              <p>Jika sudah melakukan pembayaran dan konfirmasi kepada pemilik kos,</p>
+              <p>info status pembayaran kos anda dapat anda dilihat pada bagian riwayat pesanan</p>
+              <p>dan pesanan anda akan diproses maksimal dalam 1x24 jam</p>
             </div>
           </div><!-- END COL-->
           <div class="col-md-5 col-md-offset-1 col-xs-12">
